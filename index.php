a21a1ac17d38d1fc619455862e8f216c0ca9c302
Xinyi <xchen92@u.rochester.edu>
2018-09-18 04:14:36 +0800

    haha
    haha

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include "inc/html-top.inc"; ?>
+
+	<body class="container">
+
+
+		<header>
+			<h1>3 persons</h1>
+			<div>hahaha</div>
+		</header>
+
+
+<?php include "inc/nav.inc"; ?> 
+			
+			<article>
+
+		
+			<h2>About Xinyi</h2>
+	        <figure class="xchen"> 
+				<img src="images/xinyi01.png" alt="tianjin">
+			</figure>
+
+			<p>
+				I am a digital media study undergraduate student of University of Rochester. I am also trying to major or minor in Spanish. I am that kind of person who is not really specialized or skilled in any specific field but always willing to try something new. I am not sure if it is a good thing or a bad thing to be exploring around different possibilities and not commit myself in one particular, but that is what I am right now, at least in this stage of my life. My computer science skill includes basic HTML, CSS, python. My other skills include cooking weird food, drawing grotesque, speaking a little chin-span-glish, and chin-span-urk.
+			</p>
+
+
+			<h2>Xiaojie</h2>
+	        <p>
+		I am an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that I completed my high school at Walsingham Academy in a little town named Williamsburg. And before that, I have lived and studied in Shanghai, China for 16 years. 
+
+
+			</p>
+
+			
+
+		
+
+
+            <h2>Roman</h2>
+	        <p>My name is Jonathan Roman. I'm from Tenafly, NJ and was born September 6th, 1997. I currently attend the University of Rochester with a major in Computer Science. MY hobbies include playing video games and basketball, both of which I participate in groups around campus for.</p>
+
+			
+		
+			
+			</article>
+
+
+
+<aside>
+
+</aside>
+
+
+<footer>
+
+	
+</footer>
+
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"> </script>
+
+</body>
+
+</html>
+

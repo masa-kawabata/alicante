5cac5ffdf5f7983d8a080f3d4800a17269d21581
Masa <mkawaba2@u.rochester.edu>
2018-09-26 15:53:53 -0400

    edited images

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 <?php include "inc/nav.inc"; ?> 
 			
 		<article>
-
 		
 			<h2>About Xinyi</h2>
+
 	        <figure class="xchen"> 
-				<img src="images/xinyi01.png" alt="tianjin">
+				<img src="images/tianjin.png" alt="tianjin">
 			</figure>
 
 			<p>
@@ -26,14 +26,23 @@
 
 
 			<h2>About Renjie</h2>
+
+			<figure>
+				<img src="images/jerry.png" alt="renjie">
+			</figure>
+
 	        <p>
-			Xiaojie is an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that he completed his high school at Walsingham Academy in a little town named Williamsburg. And before that, he has lived and studied in Shanghai, China for 16 years. 
+			Xiaojie is an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that he completed his high school at Walsingham Academy in a little town named Williamsburg. He has lived and studied in Shanghai, China for 16 years. 
 			</p>
 
 			<a href="rxiao2.php">Click here to get to know more about Renjie...</a>
 
 
             <h2>About Jonathan</h2>
+
+            <figure>
+            	<img src="jroman.jpg" alt="jonathan">
+            </figure>
 
 		    <p>
 		        Jonathan is from Tenafly, NJ and was born September 6th, 1997. He currently attends the University of Rochester with a major in Computer Science. His hobbies include playing video games and basketball, both of which he participates in groups around campus for.

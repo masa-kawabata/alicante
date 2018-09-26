9be501ecb239714a890633a38dd14d3f96ad4bb8
Masa <mkawaba2@u.rochester.edu>
2018-09-26 15:48:19 -0400

    reedited index
    Shortened up paragraphs, deleted unnecessary contents

--- index.php
+++ index.php
@@ -4,14 +4,13 @@
 
 
 		<header>
-			<h1>3 persons</h1>
-			<div>hahaha</div>
+			<h1>Welcome to Office Alicante!</h1>
 		</header>
 
 
 <?php include "inc/nav.inc"; ?> 
 			
-			<article>
+		<article>
 
 		
 			<h2>About Xinyi</h2>
@@ -20,27 +19,32 @@
 			</figure>
 
 			<p>
-				I am a digital media study undergraduate student of University of Rochester. I am also trying to major or minor in Spanish. I am that kind of person who is not really specialized or skilled in any specific field but always willing to try something new. I am not sure if it is a good thing or a bad thing to be exploring around different possibilities and not commit myself in one particular, but that is what I am right now, at least in this stage of my life. My computer science skill includes basic HTML, CSS, python. My other skills include cooking weird food, drawing grotesque, speaking a little chin-span-glish, and chin-span-urk.
+				Xinyi is a digital media study undergraduate student of University of Rochester. She is also trying to major or minor in Spanish. Her computer science skill includes basic HTML, CSS, python. Her other skills include cooking weird food, drawing grotesque, speaking a little chin-span-glish, and chin-span-urk.
 			</p>
 
+			<a href="xchen.php">Click here to get to know more about Xinyi...</a>
 
-			<h2>Xiaojie</h2>
+
+			<h2>About Renjie</h2>
 	        <p>
-		I am an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that I completed my high school at Walsingham Academy in a little town named Williamsburg. And before that, I have lived and studied in Shanghai, China for 16 years. 
-
-
+			Xiaojie is an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that he completed his high school at Walsingham Academy in a little town named Williamsburg. And before that, he has lived and studied in Shanghai, China for 16 years. 
 			</p>
 
-			
+			<a href="rxiao2.php">Click here to get to know more about Renjie...</a>
 
 
-            <h2>Roman</h2>
-	        <p>My name is Jonathan Roman. I'm from Tenafly, NJ and was born September 6th, 1997. I currently attend the University of Rochester with a major in Computer Science. MY hobbies include playing video games and basketball, both of which I participate in groups around campus for.</p>
+            <h2>About Jonathan</h2>
+
+		    <p>
+		        Jonathan is from Tenafly, NJ and was born September 6th, 1997. He currently attends the University of Rochester with a major in Computer Science. His hobbies include playing video games and basketball, both of which he participates in groups around campus for.
+	    	</p>
+
+	    	<a href="jroman8.php">Click here to get to know more about Jonathan...</a>
 
 			
 		
 			
-			</article>
+		</article>
 
 
 
